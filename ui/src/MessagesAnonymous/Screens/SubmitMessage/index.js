222f34f6afaae66bf6ab117a78ac4875d2c9bd96
arathyrose <arathyrose2000@gmail.com>
2019-12-31 12:56:29 +0530

    Submit message screen done TODO: add the back button

--- ui/src/MessagesAnonymous/Screens/SubmitMessage/index.js
+++ ui/src/MessagesAnonymous/Screens/SubmitMessage/index.js
@@ -1,10 +1,28 @@
 import React, { useEffect } from "react";
+import { SubmitMessageContainer, ProfilePic, UserName, Question, Message, submitButton } from './styles';
 
 function SubmitMessage(props) {
   let params = new URL(document.location).searchParams;
   let referee = params.get("ref");
   return <SubmitMessageContainer>
-    
+    <ProfilePic>
+      <img src="face.png" width="100" height="100"></img>
+    </ProfilePic>
+    <UserName>
+      John Doe
+    </UserName>
+    <Question>
+      <div>
+        Blah blah blah blah blah
+      </div>
+    </Question>
+    <Message>
+      <textarea rows="4" cols="50" autoFocus wrap="hard">
+      </textarea>
+    </Message>
+    <submitButton>
+      <button style={{ backgroundColor: '#63360D', color: "#fff" }}>SUBMIT</button>
+    </submitButton>
   </SubmitMessageContainer>;
 }
 export default SubmitMessage;
---
+++ ui/src/MessagesAnonymous/Screens/SubmitMessage/styles.js
@@ -0,0 +1,37 @@
+import styled from "styled-components";
+
+export const SubmitMessageContainer = styled.div`
+    height: 100%;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction:column;
+    font-size:36px;
+`;
+
+export const ProfilePic = styled.div`
+margin-left:20px;`;
+
+export const UserName = styled.div`
+    font-size:20;
+    padding:10px;
+
+`;
+
+export const Question = styled.div`
+    font-size:14px;
+    padding:10px;
+    padding-top:100px;
+    margin-left:10px;
+`;
+
+export const Message = styled.div`
+    margin-bottom:100px;    
+`;
+
+export const submitButton = styled.div`
+    margin-bottom:10px;
+    color: #63360D;
+    font-size:12px;
+`;
